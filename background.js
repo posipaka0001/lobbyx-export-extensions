333fbe4d4b694d63cf7922ff8313a2e60f58c99d
Kolos <kolos7720@gmail.com>
2025-08-25 15:27:31 +0300

    feat: loading icon

--- background.js
+++ background.js
@@ -2,13 +2,20 @@ let urlsToVisit = [
   // 'https://hirefire.thelobbyx.com/vacancies/689a339f138aa791a38cf3b4',
   // 'https://hirefire.thelobbyx.com/vacancies/689a3340138aa791a38cf3b3',
   // 'https://hirefire.thelobbyx.com/vacancies/68933b1a138aa791a38cea16',
-  // 'https://hirefire.thelobbyx.com/vacancies/6891f0d0ef38356c0dfe7fa4',
-  'https://hirefire.thelobbyx.com/vacancies/676eac052ccb84685fe17efb'
+  'https://hirefire.thelobbyx.com/vacancies/6891f0d0ef38356c0dfe7fa4',
+
+  // 'https://hirefire.thelobbyx.com/vacancies/676eac052ccb84685fe17efb' // Front End Developer
 ];
 let currentIndex = 0;
 let data = [];
+let scrapeInProgress = false;
 
 chrome.action.onClicked.addListener(() => {
+  if (scrapeInProgress) return;
+  scrapeInProgress = true;
+
+  showLoadingIcon();
+
   console.log('Clicked on the browser');
   currentIndex = 0;
   openNextUrl();
@@ -39,8 +46,9 @@ chrome.runtime.onMessage.addListener((msg, sender) => {
 
 function openNextUrl() {
   if (currentIndex >= urlsToVisit.length) {
-    console.log(data)
-
+    scrapeInProgress = false;
+    console.log(data);
+    showDefaultIcon();
     // chrome.runtime.sendMessage({ action: "log", data: "✅ Done scraping!" });
     return;
   }
@@ -51,6 +59,14 @@ function openNextUrl() {
       files: ["content.js"]
     });
   });
+}
+
+function showLoadingIcon() {
+  chrome.action.setIcon({ path: '/icons/loading32.png' })
+}
+
+function showDefaultIcon() {
+  chrome.action.setIcon({ path: '/icons/icon32.png' })
 }
 
 function addAllPagesToURLs(numberOfPages) {
